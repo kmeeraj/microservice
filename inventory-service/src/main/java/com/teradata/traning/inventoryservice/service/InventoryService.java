4e449317e29db620e7af8090dd596ab739ea65c4
meeraj kanaparthi <meeraj.kanaparthi@teradata.com>
2019-07-01 16:54:31 +0530

    validate DTO

--- inventory-service/src/main/java/com/teradata/traning/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/teradata/traning/inventoryservice/service/InventoryService.java
@@ -2,8 +2,12 @@ package com.teradata.traning.inventoryservice.service;
 
 import com.teradata.traning.inventoryservice.model.Inventory;
 import com.teradata.traning.inventoryservice.repository.InventoryRepository;
+import lombok.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Size;
 
 @Service
 public class InventoryService {
@@ -11,7 +15,7 @@ public class InventoryService {
     @Autowired
     InventoryRepository inventoryRepository;
 
-    public  boolean saveInventory(InventoryDTO inventoryDTO){
+    public  boolean saveInventory(@Valid InventoryDTO inventoryDTO){
         Inventory inventoryEntity = new InventoryTransformer().tranform(inventoryDTO);
         Inventory saved = inventoryRepository.save(inventoryEntity);
         return saved!=null;
@@ -19,8 +23,17 @@ public class InventoryService {
     public boolean isExists(String name) {
         return false;
     }
-    public static class InventoryDTO {
 
+    @Data
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @ToString
+    @EqualsAndHashCode
+    public static class InventoryDTO {
+        @Size(max = 100, min = 10, message ="Please provide the name in the range of 10 to 100")
+        String name;
+        String description;
     }
     private class InventoryTransformer {
         public Inventory tranform(InventoryDTO inventoryDTO){
