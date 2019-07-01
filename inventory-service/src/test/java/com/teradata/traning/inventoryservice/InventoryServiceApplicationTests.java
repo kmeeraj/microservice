79523090f0f01b66f076a08bc0a81c18a3be47f5
meeraj kanaparthi <meeraj.kanaparthi@teradata.com>
2019-07-01 16:30:23 +0530

    added test

--- inventory-service/src/test/java/com/teradata/traning/inventoryservice/InventoryServiceApplicationTests.java
+++ inventory-service/src/test/java/com/teradata/traning/inventoryservice/InventoryServiceApplicationTests.java
@@ -1,16 +1,36 @@
 package com.teradata.traning.inventoryservice;
 
+import com.teradata.traning.inventoryservice.model.Inventory;
+import com.teradata.traning.inventoryservice.repository.InventoryRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class InventoryServiceApplicationTests {
 
+    @Autowired
+    InventoryRepository inventoryRepository;
+
     @Test
     public void contextLoads() {
     }
 
+    @Test
+    public void shouldSaveInventory(){
+        Inventory inventory = Inventory.builder()
+                .name("Samsung")
+                .description("test")
+                .build();
+        inventoryRepository.save(inventory);
+        List<Inventory> allInventory = inventoryRepository.findAll();
+
+        Assertions.assertThat(allInventory.stream().findFirst().equals(inventory));
+    }
 }
